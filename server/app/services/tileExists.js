4e2ac2048306be97592cd5f4e31957703b14b7fd
Rossignol Alex <alexnw33910@gmail.com>
2024-09-02 12:20:34 +0200

    Step: 04 ✅

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,16 @@
-module.exports = null;
+const tables = require('../../database/tables');
+
+const tileExists = async(req, res , next) => {
+    const coordX = req.body.coord_x;
+    const coordY = req.body.coord_y;
+
+    const tileXY = await tables.tile.readByCoordinates(coordX, coordY);
+
+    if(tileXY.length === 0){
+        res.sendStatus(422)
+    }else{
+        next()
+    }
+};
+
+module.exports = tileExists;
