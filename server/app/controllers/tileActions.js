aebd6d79a0ef4258dc538205841530e4882a2fac
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-09-02 10:50:33 +0200

    finished step 2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readAll();
+    if (tiles == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(tiles);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { browse };
