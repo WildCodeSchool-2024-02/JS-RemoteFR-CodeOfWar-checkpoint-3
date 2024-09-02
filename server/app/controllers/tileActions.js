eb8dcb2cf8e6b39e66dc12bbae241399359da989
Benjamin <dilgopierre@gmail.com>
2024-09-02 09:44:26 +0200

    :hammer: Add GET /tile route

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,14 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const boats = await tables.tile.readAll();
+    res.status(200).json(boats);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
