57e5c46dec42156511714cea3e357930732ffe8d
ANTOINE FOUGA <antoine.fouga1@gmail.com>
2024-09-02 09:59:11 +0200

    🗃️  finished step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -6,10 +6,14 @@ CREATE TABLE boat (
   PRIMARY KEY(id)
 );
 
+
 CREATE TABLE tile (
   id INT AUTO_INCREMENT NOT NULL,
   type VARCHAR(255) NOT NULL,
+  has_treasure BOOLEAN NOT NULL DEFAULT FALSE,
   coord_x INT NOT NULL,
   coord_y INT NOT NULL,
   PRIMARY KEY(id)
 );
+
+
