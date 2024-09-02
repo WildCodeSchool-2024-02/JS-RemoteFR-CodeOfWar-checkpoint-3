68a947a047ac0bdc24bb13952bf3e9dd81dd38b6
Rosa Ben <benchabane.rosa@outlook.com>
2024-09-02 09:52:55 +0200

    step 1 done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,15 +1,17 @@
 CREATE TABLE boat (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    coord_x INT NOT NULL,
+    coord_y INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tile (
-  id INT AUTO_INCREMENT NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    coord_x INT NOT NULL,
+    coord_y INT NOT NULL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE tile ADD has_treasure BOOLEAN NOT NULL DEFAULT FALSE;
