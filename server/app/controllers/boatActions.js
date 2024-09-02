8ca264fa0e5fd4087565e90ec349e6aa28449889
dgonc <d.goncalves89000@gmail.com>
2024-09-02 10:30:05 +0200

    finished step 3 :white_check_mark:

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -2,17 +2,32 @@ const tables = require("../../database/tables");
 
 const browse = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await tables.boat.readAll();
 
-    // Respond with the boats in JSON format
     res.json(boats);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+const edit = async (req, res, next) => {
+  const boat = {
+    ...req.body,
+    coord_x: req.body.coord_x,
+    coord_y: req.body.coord_y,
+    id: req.params.id,
+  };
+
+  try {
+    await tables.boat.update(boat);
+
+    res.sendStatus(204);
+  } catch (err) {
     next(err);
   }
 };
 
 module.exports = {
   browse,
+  edit,
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -6,11 +6,18 @@ class BoatRepository extends AbstractRepository {
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await this.database.query(`select * from ${this.table}`);
 
-    // Return the array of boats
     return rows;
+  }
+
+  async update(boat) {
+    const [result] = await this.database.query(
+      `update ${this.table} set coord_x= ?, coord_y= ? where id= ?`,
+      [boat.coord_x, boat.coord_y, boat.id]
+    );
+
+    return result.affectedRows;
   }
 }
 
