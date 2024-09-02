42c61e0ba7158a891aff76bca93429cf91e4f0c3
BaptisteFredj <redj2304@gmail.com>
2024-09-02 10:14:40 +0200

    Step 2 : done

--- server/app/routers/api/tiles/router.js
+++ server/app/routers/api/tiles/router.js
@@ -3,20 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const tilesRouter = require("./tiles/router");
+const { browse } = require("../../../controllers/tileActions");
 
-router.use("/tiles", tilesRouter);
-
-const boatsRouter = require("./boats/router");
-
-router.use("/boats", boatsRouter);
-
-const gamesRouter = require("./games/router");
-
-router.use("/games", gamesRouter);
+router.get("/", browse);
 
 /* ************************************************************************* */
 
