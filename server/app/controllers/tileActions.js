ea08968ef58bc22637ae1e739c12e98ed4ffc9ae
clement Vigouroux <clement.vigouroux@gmail.com>
2024-09-02 10:11:33 +0200

    finished Step2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tile = await tables.tile.readAll();
+    res.json(tile);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {browse,};
