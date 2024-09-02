2fa1d43b12ef08fca44483770de6e05b5c7d5e75
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-09-02 10:45:37 +0200

    :white_check_mark: step2 done !

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readAll();
+
+    res.status(200).json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+module.exports = { browse };
