5b82da9ffe9a7c8a63db5b5b8c044e08ab5017b4
dgonc <d.goncalves89000@gmail.com>
2024-09-02 12:07:24 +0200

    finished step 4 :white_check_mark:

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,10 @@
-module.exports = null;
+const tileExist = (req, res, next) => {
+  console.info(req.body);
+  if (!req.body.coord_x && !req.body.coord_y) {
+    next();
+  } else if (req.body.coord_x || req.body.coord_y) {
+    res.sendStatus(422);
+  }
+};
+
+module.exports = tileExist;
