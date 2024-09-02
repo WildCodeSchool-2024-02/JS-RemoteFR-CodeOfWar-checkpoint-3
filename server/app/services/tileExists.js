131bafd20c9e9958fe509870c0c8d89d7bf9fc24
Monica Pardo <moonyruna3@gmail.com>
2024-09-02 12:40:13 +0200

    finished step 4

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,21 @@
-module.exports = null;
+const tables = require("../../database/tables")
+
+const tileExist = async (req, res, next) => {
+  const { coordX, coordY } = req.body;
+  if (coordX < 0 || coordX > 11 || coordY < 0 || coordY > 5) {
+    return res.sendStatus(422)
+  }
+
+  try {
+    const tiles = await tables.tile.readByCoordinates(coordX, coordY);
+
+    if (tiles.length === 0) {
+      return res.status(404).send({ error: 'Tuile non trouve' });
+    }
+    return next();
+  } catch (err) {
+    return next(err)
+  }
+}
+
+module.exports = tileExist;
