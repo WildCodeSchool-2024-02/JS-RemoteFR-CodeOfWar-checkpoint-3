3cb9f6e531f5dc0ab451c799f95360b64c616323
Damien <goguet.damien@hotmail.fr>
2024-09-02 12:02:03 +0200

    Step3: DONE✅

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -12,7 +12,30 @@ const browse = async (req, res, next) => {
     next(err);
   }
 };
+const edit = async (req, res, next) => {
+  try {
+    const boatId = req.params.id;
+
+    const boatUpdate = {
+      id: boatId,
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await tables.boat.update(boatUpdate);
+    if (affectedRows > 0) {
+      res.sendStatus(204);
+    } else {
+      // Respond with the boats in JSON format
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
 
 module.exports = {
   browse,
+  edit,
 };
